657ed5d40b70ec0d6fe79fdef6356cd7f102690e
Rafal Kluszczynski <klusi@icm.edu.pl>
2017-04-21 12:41:15 +0200

    update build gradle files

--- pmf-runner/src/test/java/pl/edu/icm/pmf/runner/ApplicationTests.java
+++ pmf-runner/src/test/java/pl/edu/icm/pmf/runner/ApplicationTests.java
@@ -2,15 +2,21 @@ package pl.edu.icm.pmf.runner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.util.Objects.nonNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+    @Autowired
+    ApplicationContext context;
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+        assert nonNull(context);
+    }
 }
